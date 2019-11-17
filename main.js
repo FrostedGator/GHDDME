0b9e5b6fea3d7ecafa4a54933e9b3090a866963b
Alvar Eschbaugh <eschbaugh.ra@ufl.edu>
2019-11-16 21:34:53 -0500

    Dark DEM Assignment
    Some edits were made.

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 function createMap() {
 
     //opening statement URLs
-    var mbAttr = '<a href="http://openstreetmap.org">OpenStreetMap</a> |' + ' <a href="http://mapbox.com">Mapbox</a> <a href="https://wooly-gator-kupo.github.io/">UFO2019 Robert Eschbaugh</a>';
+    var mbAttr = '<a href="http://openstreetmap.org">OpenStreetMap</a> |' + ' <a href="http://mapbox.com">Mapbox</a> |' + '  <a href="https://wooly-gator-kupo.github.io/">UFO2019 Robert Eschbaugh</a>';
     
     //Mapbox API Token
-    var apitoken = 'pk.eyJ1Ijoid29vbHktZ2F0b3Ita3VwbyIsImEiOiJjazJvM2l3MG0wN2IxM2dwYnBkeXMwMzNvIn0.t2SQOlFI55bLWqBUqfJNMg'; //Enter your API Token - go to 'https://www.mapbox.com/install/' to yours */
+    var apitoken = 'pk.eyJ1Ijoid29vbHktZ2F0b3Ita3VwbyIsImEiOiJjazJvM2l3MG0wN2IxM2dwYnBkeXMwMzNvIn0.t2SQOlFI55bLWqBUqfJNMg'; 
     
     //Stanard MaxBox Styles URL
     var mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={token}';
@@ -14,7 +14,7 @@ function createMap() {
     var mbStyleUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={token}'; s
     
     //custom basemaps
-    var customeBasemap = L.tileLayer(mbStyleUrl, {id: 'wooly-gator-kupo/ck2o3oae50d451cpbnf46lpe2', token: apitoken,  attribution: mbAttr});
+    var customBasemap = L.tileLayer(mbStyleUrl, {id: 'wooly-gator-kupo/ck2o3oae50d451cpbnf46lpe2', token: apitoken,  attribution: mbAttr});
     
     //different basemap variables
     var MapBox   = L.tileLayer(mbUrl, {id: 'mapbox.light', token: apitoken, attribution: mbAttr});
@@ -23,7 +23,7 @@ function createMap() {
     
     //maps to be created
     var map = L.map('map', {
-        center: [36.00, -5],//Coordinated to center the map
+        center: [36.00, -5],//map center coordination 
         zoom: 7, //zoom level
         layers: Dark //dark base
     });
@@ -37,7 +37,7 @@ function createMap() {
     
     L.control.layers(baseLayers).addTo(map);
     
-};
+}
 
 //default call function
 $(document).ready(createMap);
