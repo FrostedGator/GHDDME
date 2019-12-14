77ec92f72b9b20cf298bc71984778498a8e84b25
Robert <54428499+FrostedGator@users.noreply.github.com>
2019-12-13 20:37:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,7 @@
 function createMap() {
 
     //opening statement URLs
-    var mbAttr = '<a href="http://openstreetmap.org">OpenStreetMap</a> |' + ' <a href="http://mapbox.com">Mapbox</a> |  <a href="https://wooly-gator-kupo.github.io/">UFO2019 Robert Eschbaugh</a>';
-    // removed "' + '" before URL
+    var mbAttr = '<a href="http://openstreetmap.org">OpenStreetMap</a> |' + ' <a href="http://mapbox.com">Mapbox</a> |' + '  <a href="https://wooly-gator-kupo.github.io/">UFO2019 Robert Eschbaugh</a>';
 	
     //Mapbox API Token
     var apitoken = 'pk.eyJ1Ijoid29vbHktZ2F0b3Ita3VwbyIsImEiOiJjazJvM2l3MG0wN2IxM2dwYnBkeXMwMzNvIn0.t2SQOlFI55bLWqBUqfJNMg'; 
